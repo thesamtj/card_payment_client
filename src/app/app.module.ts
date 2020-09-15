b2388c4d86757449586699ef4b4bb130b43b26cc
Samuel Tijani <omalitijani@gmail.com>
2020-09-15 16:33:24 +0100

    Create payment detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailComponent
   ],
   imports: [
     BrowserModule,
