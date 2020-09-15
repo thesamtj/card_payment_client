c22b566b4d2baa4e1c1f373dc5d9ae5d0cbe509a
Samuel Tijani <omalitijani@gmail.com>
2020-09-15 16:34:46 +0100

    Add payment detail list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
+import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentDetailComponent
+    PaymentDetailComponent,
+    PaymentDetailListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/payment-details/payment-detail-list/payment-detail-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detail-list',
+  templateUrl: './payment-detail-list.component.html',
+  styleUrls: ['./payment-detail-list.component.scss']
+})
+export class PaymentDetailListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
