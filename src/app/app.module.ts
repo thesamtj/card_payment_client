1b49767fa686b2140b9a923360a4bd23a48e0248
Samuel Tijani <omalitijani@gmail.com>
2020-09-15 17:11:10 +0100

    Create a simple form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
@@ -15,6 +16,7 @@ import { PaymentDetailService } from './shared/payment-detail.service';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [PaymentDetailService],
