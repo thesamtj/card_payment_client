9cebbd9bba863801a51528cb343957c951be1d85
Samuel Tijani <omalitijani@gmail.com>
2020-09-16 10:10:50 +0100

    Install toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,7 +17,14 @@ import { PaymentDetailService } from './shared/payment-detail.service';
     PaymentDetailComponent,
     PaymentDetailListComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+  ],
   providers: [PaymentDetailService],
   bootstrap: [AppComponent],
 })
