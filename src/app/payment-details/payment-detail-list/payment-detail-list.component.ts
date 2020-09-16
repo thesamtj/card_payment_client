0c8583c2a2243db31de9c36b260a6438a1007407
Samuel Tijani <omalitijani@gmail.com>
2020-09-16 11:10:11 +0100

    Repopulate form

--- src/app/payment-details/payment-detail-list/payment-detail-list.component.ts
+++ src/app/payment-details/payment-detail-list/payment-detail-list.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+import { PaymentDetail } from 'src/app/model/payment-detail';
 
 @Component({
   selector: 'app-payment-detail-list',
   templateUrl: './payment-detail-list.component.html',
-  styleUrls: ['./payment-detail-list.component.scss']
+  styleUrls: ['./payment-detail-list.component.scss'],
 })
 export class PaymentDetailListComponent implements OnInit {
-
-  constructor(public service: PaymentDetailService) { }
+  constructor(public service: PaymentDetailService) {}
 
   ngOnInit(): void {
     this.service.refreshList();
   }
 
-  refreshList() {
-
+  populateForm(pd: PaymentDetail) {
+    this.service.formData = Object.assign({}, pd);
   }
-
 }
